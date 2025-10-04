12b681417646d4c899c8f608c7a31e748f818dce
NadavKeren <nadav.keren@gmail.com>
2025-10-04 13:23:18 +0300

    Added Untested Implementations of the pipeline blocks

--- src/pipeline_block.hpp
+++ src/pipeline_block.hpp
@@ -3,8 +3,9 @@
 #include <cstdint>
 #include <string>
 #include <optional>
-#include <array>
-#include "constants.hpp"
+#include <vector>
+#include <iostream>
+
 #include "utils.cpp"
 #include "fixed_size_array.hpp"
 
@@ -17,13 +18,18 @@ struct EntryData {
     EntryData() : id(0), latency(0.0), tokens(0), last_access_time(0) {}
 };
 
-using NewLocationData = std::array<std::pair<uint64_t, uint64_t>, Constants::QUANTUM_SIZE>;
+using NewLocationData = std::vector<std::pair<uint64_t, uint64_t>>;
+
+struct QuantumMoveResult {
+    NewLocationData items_moved;
+    NewLocationData items_remaining;
+};
 
 class PipelineBlock {
 public:
     virtual ~PipelineBlock() = default;
     // Using the Visitor pattern to allow direct memcpy.
-    virtual NewLocationData move_quanta_to(PipelineBlock& other) = 0;
+    virtual QuantumMoveResult move_quanta_to(PipelineBlock& other) = 0;
     virtual NewLocationData accept_quanta(FixedSizeArray<EntryData>& arr) = 0;
 
     virtual FixedSizeArray<EntryData>& get_arr() = 0;
@@ -32,6 +38,7 @@ public:
     virtual uint64_t capacity() const = 0;
     virtual bool is_full() const = 0;
     virtual EntryData* get_entry(uint64_t idx) = 0;
+    virtual void prepare_for_copy() = 0;
     virtual std::string get_type() const = 0;
     virtual void clear() = 0;
 };
@@ -45,39 +52,56 @@ protected:
     uint64_t m_curr_max_capacity;
     const std::string m_type;
 public:
-    BasePipelineBlock(uint64_t cache_capacity, 
-                      uint64_t quantum_size, 
-                      uint64_t curr_quanta_alloc, 
+    BasePipelineBlock(uint64_t cache_capacity,
+                      uint64_t quantum_size,
+                      uint64_t curr_quanta_alloc,
                       const std::string& type) : m_arr{cache_capacity},
                                                  m_cache_max_capacity {cache_capacity},
                                                  m_quantum_size{quantum_size},
                                                  m_curr_max_capacity{m_quantum_size * curr_quanta_alloc},
                                                  m_type {type}
                                                  {}
-    
+
     BasePipelineBlock(const BasePipelineBlock& other) : m_arr{other.m_arr},
-                                                        m_cache_max_capacity{other.m_curr_max_capacity},
+                                                        m_cache_max_capacity{other.m_cache_max_capacity},
                                                         m_quantum_size{other.m_quantum_size},
                                                         m_curr_max_capacity{other.m_curr_max_capacity},
                                                         m_type{other.m_type}
                                                   {}
+
+    BasePipelineBlock& operator=(const BasePipelineBlock& other)
+    {
+        m_arr = other.m_arr;
+        assert(m_cache_max_capacity == other.m_cache_max_capacity);
+        assert(m_quantum_size == other.m_quantum_size);
+        m_curr_max_capacity = other.m_curr_max_capacity;
+
+        return *this;
+    }
     
     virtual ~BasePipelineBlock() = default;
 
     
-    NewLocationData accept_quanta(FixedSizeArray<EntryData>& arr) 
+    NewLocationData accept_quanta(FixedSizeArray<EntryData>& arr) override
     {
-        assert(arr.size() + this->m_quantum_size <= arr.capacity());
-        this->m_arr.rotate();
-        arr.partial_move_to(m_arr, this->m_quantum_size);
-        NewLocationData locations{};
-        const uint64_t offset = arr.size() - this->m_quantum_size;
-        for (uint64_t i = 0; i < this->m_quantum_size; ++i) {
-            locations[i] = std::make_pair(arr[offset + i].id, offset + i);
-        }
-
         this->m_curr_max_capacity += this->m_quantum_size;
         assert(this->m_curr_max_capacity <= this->m_cache_max_capacity);
+
+        NewLocationData locations{};
+        if (!arr.empty())
+        {
+            assert(arr.size() + this->m_quantum_size <= arr.capacity());
+            this->m_arr.rotate();
+            
+            const uint64_t dest_start_idx = m_arr.size();
+            arr.partial_move_to(m_arr, this->m_quantum_size);
+
+            for (uint64_t i = 0; i < this->m_quantum_size; ++i) 
+            {
+                locations.emplace_back(m_arr[dest_start_idx + i].id, dest_start_idx + i);
+            }
+        }
+
 
         return locations;
     }
@@ -94,6 +118,11 @@ public:
     {
         assert(idx >= 0 && idx < m_arr.size());
         return m_arr.get_item(idx);
+    }
+
+    void prepare_for_copy() override
+    {
+        m_arr.rotate();
     }
 
     std::string get_type() const override { return m_type; }
